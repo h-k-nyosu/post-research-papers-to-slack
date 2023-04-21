4417ead0e3e6a91f9185753f9f2a5b096f667017
h-k-nyosu <kuro.h1020@gmail.com>
2023-04-22 06:44:00 +0900

    fix: キーワードの変更

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ SLACK_CHANNEL = os.environ["SLACK_CHANNEL"]
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 DATABASE_NAME = "papers.db"
 
-SEARCH_KEYWORDS = ["LLM", "GPT", "LFM", "LLMM"]
+SEARCH_KEYWORDS = ["LLM", "GPT", "LFM", "prompt"]
 SEARCH_AUTHORS = []  # ["John Doe", "Jane Smith"] のように検索する著者名を追加できます（完全一致検索）
